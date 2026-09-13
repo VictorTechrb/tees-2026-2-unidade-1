8b6a2aad987b5b6f892686d2c87766e2207fddca
yasmim-passos <passosyasmim53@gmail.com>
2026-09-13 18:51:39 -0300

    refactor: move BibliotecaContext e Identity de Core para Adapters
    O contexto do Entity Framework e o Identity saem do dominio e passam
    para o projeto de infraestrutura. O OnModelCreating nao foi tocado: os
    arquivos mudam de lugar, nao de conteudo.
    
    O namespace Core.Identity.Data vira Adapters.Identity, para pasta e
    namespace ficarem coerentes; os 31 consumidores tiveram apenas a linha
    de using atualizada. Autenticacao esta fora do escopo arquitetural
    deste trabalho - mover era necessario para esvaziar o Core.

--- exercicio-1-arquitetura/projeto-es-biblioteca-2025-hexagonal/Codigo2025/Biblioteca/Adapters/Identity/IdentityContext.cs
+++ exercicio-1-arquitetura/projeto-es-biblioteca-2025-hexagonal/Codigo2025/Biblioteca/Adapters/Identity/IdentityContext.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Core.Identity.Data;
+namespace Adapters.Identity;
 
 public class IdentityContext : IdentityDbContext<UsuarioIdentity>
 {
--- exercicio-1-arquitetura/projeto-es-biblioteca-2025-hexagonal/Codigo2025/Biblioteca/BibliotecaWeb/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ exercicio-1-arquitetura/projeto-es-biblioteca-2025-hexagonal/Codigo2025/Biblioteca/BibliotecaWeb/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Core.Identity.Data;
+using Adapters.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
